792f862238d2811ce1f1d9748ae069ad6dbf2117
poushitaguha <pguha.kolkata@gmail.com>
2019-05-15 13:13:51 +0000

    Add loopy lighthouse exercise

---
+++ loopy-lighthouse.js
@@ -0,0 +1,13 @@
+var startNum = 100;
+var endNum = 200;
+for (var i = startNum; i <= endNum; i++) {
+  if (i % 3 === 0 && i % 4 === 0) {
+    console.log("LoopyLighthouse");
+  } else if (i % 3 === 0) {
+    console.log("Loopy");
+  } else if (i % 4 === 0) {
+    console.log("Lighthouse");
+  } else {
+    console.log(i);
+  }
+}
